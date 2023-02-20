986e340cd2f6aa65b10b257f4f942cd3971c6052
Leandro Doctors <ldoctors@gmail.com>
2023-02-19 22:51:26 -0300

    Import scramble solution (backend)

--- src/allentiak/scramble.clj
+++ src/allentiak/scramble.clj
@@ -1,5 +1,41 @@
 (ns allentiak.scramble
-  (:gen-class))
+  (:gen-class)
+  (:require
+    [clojure.string :as str]))
+
+
+(defn- rearrangable?
+  "returns true if a portion of letters can be rearranged to match word, otherwise returns false.
+  Worst possible case scenario comparison.
+  Only considers lowercase a-z characters."
+  [letters word]
+  (cond
+    (nil? word) true
+    (not (str/includes? letters (str (first word)))) false
+    :else (rearrangable? letters (seq (rest word)))))
+
+(comment
+  (rearrangable? "ab" "b"))
+
+
+(defn scramble?
+  "returns true if a portion of letters can be rearranged to match word, otherwise returns false.
+  Efficient version.
+  Only considers lowercase a-z characters."
+  [letters word]
+  (cond
+    (or (nil? letters)
+        (nil? word)
+        (< (count letters) (count word)))
+    false
+
+    (or (= letters word)
+        (str/includes? letters word))
+    true
+
+    :else
+    (rearrangable? letters word)))
+
 
 (defn greet
   "Callable entry point to the application."
--- test/allentiak/scramble_test.clj
+++ test/allentiak/scramble_test.clj
@@ -1,7 +1,46 @@
 (ns allentiak.scramble-test
-  (:require [clojure.test :refer :all]
-            [allentiak.scramble :refer :all]))
+  (:require
+    [allentiak.scramble :refer [scramble?]]
+    [clojure.test :refer [deftest is testing]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest scramble-test
+
+  (testing "exceptional behaviors"
+    ;; nil strings make the result false
+    (is (= (scramble? nil nil) false))
+    (is (= (scramble? nil "") false))
+    (is (= (scramble? "" nil) false)))
+
+  (testing "degenerate case"
+    ;; the second string should always be smaller or equal than the second one
+    (is (= (scramble? "ab" "abc") false))
+    (is (= (scramble? "" "a") false)))
+
+  (testing "basic cases"
+    ;; the same string should always match, even if it's blank
+    (is (= (scramble? "a" "a") true))
+    (is (= (scramble? "abc" "abc") true))
+    (is (= (scramble? "" "") true)))
+
+  (testing "strict subset case"
+    (is (= (scramble? "abc" "") true))
+    (is (= (scramble? "abc" "a") true))
+    (is (= (scramble? "abc" "b") true))
+    (is (= (scramble? "abc" "c") true))
+    (is (= (scramble? "abc" "ab") true))
+    (is (= (scramble? "abc" "bc") true)))
+
+  (testing "second element is blank"
+    (is (= (scramble? "a" "") true)))
+
+  (testing "non-strict subset case"
+    (is (= (scramble? "abc" "ac") true))
+    (is (= (scramble? "abc" "ba") true))
+    (is (= (scramble? "abc" "ca") true))
+    (is (= (scramble? "abc" "cb") true)))
+
+  (testing "provided cases"
+    (is (= (scramble? "rekqodlw" "world") true))
+    (is (= (scramble? "cedewaraaossoqqyt" "codewars") true))
+    (is (= (scramble? "katas" "steak") false))))
