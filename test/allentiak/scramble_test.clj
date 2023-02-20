02d9f5aa43485a2f09a21a2d5edf39c65ff740a0
Leandro Doctors <ldoctors@gmail.com>
2023-02-19 22:35:52 -0300

    Create project witn 'clj-new'
    % clojure -Tnew app :name allentiak/scramble

---
+++ test/allentiak/scramble_test.clj
@@ -0,0 +1,7 @@
+(ns allentiak.scramble-test
+  (:require [clojure.test :refer :all]
+            [allentiak.scramble :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
