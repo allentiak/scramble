50150210e1af8d53ca18152b18b86bf1aeb37f38
Leandro Doctors <ldoctors@gmail.com>
2023-02-21 12:57:14 -0300

    fixup! fixup! add CLI interface, with clojure/tools.cli (WIP)

--- src/allentiak/scramble.clj
+++ src/allentiak/scramble.clj
@@ -1,19 +1,21 @@
 (ns allentiak.scramble
   (:gen-class)
   (:require
-   [allentiak.scramble.backend :as backend]
-   [clojure.string :as str]
-   [clojure.tools.cli :refer [parse-opts]]))
+    [clojure.string :as str]
+    [clojure.pprint :as pprint]
+    [clojure.tools.cli :refer [parse-opts]]
+    [allentiak.scramble.backend :as backend]))
 
 (def cli-options
   [["-l" "--letters LETTERS" "Letters"
     :required "Must be provided"
-    :validate [#(not (str/blank?)) "Must not be empty"]]
+    :validate [#(not (str/blank? %)) "Must not be empty"]]
    ["-w" "--word WORD" "Word"
     :required "Must be provided"
-    :validate [#(not (str/blank?)) "Must not be empty"]]])
+    :validate [#(not (str/blank? %)) "Must not be empty"]]])
 
 (defn -main
   "Callable entry point of the application."
   [& args]
+  (pprint/pprint (parse-opts args cli-options))
   (apply backend/scramble? (parse-opts args cli-options)))
