22536c27c954c7d68dbc3de51ce8e828ec86051a
Leandro Doctors <ldoctors@gmail.com>
2023-04-11 11:48:48 -0300

    scramble: REST API: add endpoint tests
    Based on Simon Polak's version:
    https://github.com/smnplk/scramblies.git

--- test/allentiak/scramble/rest_api_test.clj
+++ test/allentiak/scramble/rest_api_test.clj
@@ -2,7 +2,8 @@
   (:require
     [allentiak.scramble.rest-api :refer [webapp landing-page]]
     [clojure.test :refer [deftest is testing]]
-    [ring.mock.request :as mock]))
+    [ring.mock.request :as mock]
+    [ring.util.codec :refer [form-encode]]))
 
 (deftest webapp-smoke-test
   (testing "always valid route"
@@ -10,8 +11,40 @@
           response (webapp req)]
       (is (= 200 (:status response)))
       (is (= (landing-page) (:body response)))))
-
   (testing "invalid route"
     (let [req (mock/request :get "/whatever")
           response (webapp req)]
       (is (= 404 (:status response))))))
+
+(def ^:private mocked-post-request
+  (-> (mock/request :post "/scramble")
+      (mock/content-type "application/x-www-form-urlencoded")))
+
+(deftest endpoint-test
+  (testing "well-formed POST request with valid params"
+    (let [response (webapp (-> mocked-post-request
+                               (mock/body (form-encode {:letters "abc" :word "abc"}))))]
+      (is (= 200 (:status response)))))
+  (testing "parameterless POST request"
+    (let [response (webapp (-> mocked-post-request
+                               (mock/body (form-encode {}))))]
+      (is (= 400 (:status response)))))
+  (testing "well-formed POST request with invalid params"
+    (let [response (webapp (-> mocked-post-request
+                               (mock/body (form-encode {:letters "" :word "  "}))))]
+      (is (= 422 (:status response))))))
+
+;; (run-tests)
+
+;; Testing on cmd via curl
+;; 1 - Well formed request with valid data
+;; curl -i -H "Accept:application/transit+json" -X POST localhost:3000/scramblie -F "letters=abc" -F "word=abc"
+;; 200
+
+;; 2 - Well formed request with invalid data (empty strings)
+;; curl -i -H "Accept:application/transit+json" -X POST localhost:3000/scramblie -F "letters=" -F "word="
+;; 422
+
+;; 2 - Invalid request, params not set
+;; curl -i -H "Accept:application/transit+json" -X POST localhost:3000/scramblie
+;; 400
