060936493ccd76598d75ed2a4214821edb59b503
Leandro Doctors <ldoctors@gmail.com>
2023-04-11 13:16:04 -0300

    scramble: rewrite CLI tests
    We are testing *against the backend*, not against an arbitrary result.

--- test/allentiak/scramble/cli_test.clj
+++ test/allentiak/scramble/cli_test.clj
@@ -1,9 +1,16 @@
 (ns allentiak.scramble.cli-test
-  (:require [allentiak.scramble.cli :as cli]
-            [clojure.test :refer [deftest is testing]]
-            [clojure.string :as str]))
+  (:require
+    [allentiak.scramble.backend :as backend]
+    [allentiak.scramble.cli :as cli]
+    [clojure.test :refer [deftest is testing]]
+    [clojure.string :as str]))
 
 (deftest main-test
   (testing "invoking -main"
-    (is (= true (cli/-main "--letters" "rekqodlw" "--word" "world")))
-    (is (= false (cli/-main "--letters" "katas" "--word" "steak")))))
+    (let [letters1 "rekqodlw"
+          word1 "world"
+          letters2 "katas"
+          word2 "steak"]
+      ;; each test case should expect a different value (respectively, true and false).
+      (is (= (backend/scramble? letters1 word1) (cli/-main "--letters" letters1 "--word" word1)))
+      (is (= (backend/scramble? letters2 word2) (cli/-main "--letters" letters2 "--word" word2))))))
