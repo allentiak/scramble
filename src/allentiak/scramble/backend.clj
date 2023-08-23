7f7bb894263d5c261a52f730546c6106a6d5e7d2
Leandro Doctors <ldoctors@gmail.com>
2023-08-23 20:59:50 +0200

    formatting: docstrings should wrap at 80 chars. Use caps!
    Thanks, Felipe!

--- src/allentiak/scramble/backend.clj
+++ src/allentiak/scramble/backend.clj
@@ -4,24 +4,25 @@
 
 
 (defn- rearrangable?
-  "returns true if a portion of letters can be rearranged to match word, otherwise returns false.
-  Worst possible case scenario comparison.
-  Only considers lowercase a-z characters."
+  "Returns true if a portion of letters can be rearranged to match word, otherwise
+  returns false.
+
+  Worst possible case scenario comparison. Only considers lowercase a-z characters."
   [letters word]
   (cond
     (nil? word) true
     (not (str/includes? letters (str (first word)))) false
     :else (rearrangable? letters (seq (rest word)))))
 
-
 (comment
   (rearrangable? "ab" "b"))
 
 
 (defn scramble?
-  "returns true if a portion of letters can be rearranged to match word, otherwise returns false.
-  Efficient version.
-  Only considers lowercase a-z characters."
+  "Returns true if a portion of letters can be rearranged to match word, otherwise
+  returns false.
+
+  Efficient version. Only considers lowercase a-z characters."
   [letters word]
   (cond
     (or (nil? letters)
