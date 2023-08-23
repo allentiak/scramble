ce3707e2b95398d1399efdd406c94d33e33e267c
Leandro Doctors <ldoctors@gmail.com>
2023-08-23 14:51:44 +0200

    fixup! refactor: REST API: extract server startup to user ns

--- dev/user.clj
+++ dev/user.clj
@@ -8,17 +8,18 @@
 
 (defn start-web-server []
   (if (nil? @server)
-    ((reset! server
-       (jetty/run-jetty #'rest-api/webapp {:port 3000, :join? false}))
-     (println "Development HTTP Server running on port 3000..."))
+    (do
+      (reset! server
+        (jetty/run-jetty #'rest-api/webapp {:port 3000, :join? false}))
+      (println "Development HTTP Server running on port 3000..."))
     (println "Cannot start server: Server already running!")))
 
 (defn stop-web-server []
   (if-not (nil? @server)
-    ((@server :timeout 100)
-     (.stop @server)
-     (reset! server nil)
-     (println "Server stopped."))
+    (do
+      (.stop @server)
+      (reset! server nil)
+      (println "Server stopped."))
     (println "Cannot stop server: Server not running!")))
 
 (defn restart-web-server []
