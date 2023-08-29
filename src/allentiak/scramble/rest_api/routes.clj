5b27a6f6c218f1c61507e6b282500561b6ada490
Leandro Doctors <ldoctors@gmail.com>
2023-08-29 13:55:54 +0200

    REST API: refactor: extract routes

--- src/allentiak/scramble/rest_api/routes.clj
+++ src/allentiak/scramble/rest_api/routes.clj
@@ -2,9 +2,11 @@
   (:require
    [allentiak.scramble.rest-api.examples :as examples]
    [allentiak.scramble.rest-api.handlers :as handlers]
-   [allentiak.scramble.rest-api.specs :as specs]))
+   [allentiak.scramble.rest-api.specs :as specs]
+   [reitit.openapi :as openapi]
+   [reitit.swagger :as swagger]))
 
-(def scramble-api-route
+(def scramble
   ["/scramble"
    {:tags ["scramble"]
     :get
@@ -31,3 +33,17 @@
         examples/request}}
       :responses
       examples/response-200}}}])
+
+(def api-documentation
+  [["/openapi.json"]
+   {:get {:no-doc  true
+          :openapi {:info {:title "my-api"
+                           :description "openapi3 docs with [malli](https://github.com/metosin/malli) and reitit-ring"
+                           :version "0.0.1"}}
+          :handler (openapi/create-openapi-handler)}}
+   ["/swagger.json"
+    {:get {:no-doc  true
+           :swagger {:info {:title "my-api"
+                            :description "swagger docs with [malli](https://github.com/metosin/malli) and reitit-ring"
+                            :version "0.0.1"}}
+           :handler (swagger/create-swagger-handler)}}]])
