b7cd990043cac97b2cbb6f261a94314efea9967b
Leandro Doctors <ldoctors@gmail.com>
2023-08-30 23:17:09 +0200

    REST API: implement web form (partially working)
    It (seems to) send the request, but it does not show the response...

--- src/allentiak/scramble/rest_api/static_content.clj
+++ src/allentiak/scramble/rest_api/static_content.clj
@@ -1,5 +1,24 @@
-(ns allentiak.scramble.rest-api.static-content)
+(ns allentiak.scramble.rest-api.static-content
+  (:require
+   [hiccup.page :as p]
+   [hiccup.form :as f]))
 
 (defn homepage
   []
-  "Hi, Static Content!")
+  (p/html5
+   [:head
+    [:title "Simple form to test my API"]]
+   [:body
+    [:h1 "Testing scramble API..."]
+    (f/form-to [:post "/scramble"]
+               [:div
+                [:label {:for "letters"} "Letters:"]
+                [:input {:type "text" :id "letters" :name "letters"}]]
+               [:div
+                [:label {:for "word"} "Word:"]
+                [:input {:type "text" :id "word" :name "word"}]]
+               [:div
+                [:input {:type "submit" :value "Submit"}]])]))
+
+(comment
+  (doc f/text-field))
